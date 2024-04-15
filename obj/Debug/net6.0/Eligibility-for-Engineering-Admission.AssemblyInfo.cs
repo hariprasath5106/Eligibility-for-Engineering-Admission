74424f90af435b79c255e901aeb58e2f240dcf31
hariprasath5106 <hariprasath.10c@gmail.com>
2024-04-15 19:41:26 +0530

    firstcommit

--- obj/Debug/net6.0/Eligibility-for-Engineering-Admission.AssemblyInfo.cs
+++ obj/Debug/net6.0/Eligibility-for-Engineering-Admission.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eligibility-for-Engineering-Admission")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cc106eb7d2578c82307d907108f43bca6d064fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eligibility-for-Engineering-Admission")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eligibility-for-Engineering-Admission")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
